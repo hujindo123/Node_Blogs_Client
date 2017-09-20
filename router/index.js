41f7a5d7bb88d01c30515bab679d587051bf395e
jd_soft@163.com <hjd13122434>
2017-09-20 17:12:51 +0800

    2017-9-20

--- router/index.js
+++ router/index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router();
 const Captchas = require('../common/captchapng');
 const Admin = require('../controller/admin/index');
-
+const upload = require('../middleware/upload');
 router.get('/getCaptchas', Captchas.getCaptchas); // 生成验证码
 router.get('/register', Admin.register);
 router.get('/login', Admin.login);
@@ -14,5 +14,6 @@ router.get('/actives', Admin.activeAccount); // 激活账号
 router.get('/updateEmailCode', Admin.updateEmailCode); //再次发送邮箱激活码
 router.get('/findPass', Admin.findPass); // 邮箱查找密码
 router.get('/updatePass', Admin.updatePass); //修改密码
+router.get('/upload', upload.getToken)
 
 module.exports = router;
